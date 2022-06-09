88f983fb2644852c88c501429d83ca06ce99c114
Diego Carlino <diego_carlino@outlook.com>
2022-06-09 21:59:19 +0200

    i am rich

---
+++ i am rich/i am rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  i am rich
+//
+//  Created by Diego Carlino on 09/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
